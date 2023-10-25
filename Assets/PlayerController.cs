75441a5b6da5803a07af127292ebfb33fd9ecf69
krunal <krunalvaghani07.com>
2023-10-25 15:59:30 -0400

    Bouncing Ball

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     float playerSpeed = 3f;
+    public Transform targetTransform;
+    bool isHitting;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +18,28 @@ public class PlayerController : MonoBehaviour
     {
         float horizontalMove = Input.GetAxisRaw("Horizontal");
         float verticalMove = Input.GetAxisRaw("Vertical");
-        if (horizontalMove != 0 || verticalMove != 0)
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            isHitting = true;
+        }else if (Input.GetKeyDown(KeyCode.H))
+        {
+            isHitting= false;
+        }
+        if (isHitting)
+        {
+            targetTransform.Translate(new Vector3 (horizontalMove, 0,0) * playerSpeed * Time.deltaTime);
+        }
+        if ((horizontalMove != 0 || verticalMove != 0) && !isHitting)
         {
             transform.Translate(new Vector3 (horizontalMove,0 ,verticalMove) * playerSpeed * Time.deltaTime);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            Vector3 direction = targetTransform.position - transform.position;
+            other.GetComponent<Rigidbody>().velocity = direction.normalized * 13 + new Vector3(0,6,0);
         }
     }
 }
